0614ecfc74604fef370127e2f46908afef8cff6d
Thomas-Tran-UMKC <ttnn3@mail.umkc.edu>
2023-06-22 17:39:29 -0500

    update to use modern OpenGL 4 followed Cherno's tutorial added link and validate error checking

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -2,6 +2,71 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, NULL);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result != GL_TRUE)
+    {
+        //std::cout << "Shader Compiler Error! \n";
+
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+
+        char* message = static_cast<char*>(alloca(length * sizeof(char)));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!\n";
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    int linkResult, validateResult;
+    glGetProgramiv(program, GL_LINK_STATUS, &linkResult);
+    glGetProgramiv(program, GL_VALIDATE_STATUS, &validateResult);
+
+    if (linkResult != GL_TRUE || validateResult != GL_TRUE)
+    {
+        int length;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &length);
+
+        char* message = static_cast<char*>(alloca(length * sizeof(char)));
+        glGetProgramInfoLog(program, length, &length, message);
+        std::cout << "Error in creating Program! \n    " << message << std::endl;
+
+    }
+
+    glDetachShader(program, vs);
+    glDeleteShader(vs);
+
+    glDetachShader(program, fs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
+
+
 int main(void)
 {
     GLFWwindow* window;
@@ -27,18 +92,59 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    float positions[6] = {
+        -.5f, -.5f,
+        0.0f,  .5f,
+         .5f, -.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    std::string vertexShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "     gl_Position = position;\n"
+        "}\n"
+        ;
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "     color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n"
+        ;
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-.5f, -.5f);
-        glVertex2f(0.0f,  .5f);
-        glVertex2f( .5f, -.5f);
-        glEnd();
 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        const char* error;
+        glfwGetError(&error);
+        if (error)
+            std::cout << error << std::endl;
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -46,6 +152,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+    
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
