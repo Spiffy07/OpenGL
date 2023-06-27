a60881482ba12b39be0add9641bb3dba6abe74ed
Thomas-Tran-UMKC <ttnn3@mail.umkc.edu>
2023-06-27 18:44:27 -0500

    Abstract Vertex arrays (objects) away from main( ) and implemented layout handling.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -10,6 +10,7 @@
 
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
+#include "VertexArray.h"
 
 struct ShaderProgramSource
 {
@@ -154,14 +155,13 @@ int main(void)
             2, 3, 0
         };
 
-        unsigned int vao;
-        GLCall(glGenVertexArrays(1, &vao));
-        GLCall(glBindVertexArray(vao));
-
+        
+        VertexArray va;
         VertexBuffer vb(positions, 4 * 2 * sizeof(float));
 
-        GLCall(glEnableVertexAttribArray(0));
-        GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));  // this links current array buffer to current VAO
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
 
         IndexBuffer ib(indicies, 6);
 
@@ -173,8 +173,7 @@ int main(void)
         ASSERT(location != -1);
         GLCall(glUniform4f(location, .0f, 1.0f, .0f, 1.0f));
 
-
-        GLCall(glBindVertexArray(0));
+        va.Unbind();
         GLCall(glUseProgram(0));
         GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
         GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
@@ -192,7 +191,7 @@ int main(void)
             GLCall(glUseProgram(shader));
             GLCall(glUniform4f(location, 0.1f, 0.1f, blueChannel, 1.0f));
 
-            GLCall(glBindVertexArray(vao));
+            va.Bind();
             ib.Bind();
 
             GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
