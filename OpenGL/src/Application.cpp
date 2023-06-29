c26b6ea7799c6e5c3dfca68972da87c6d0f94c40
Thomas-Tran-UMKC <ttnn3@mail.umkc.edu>
2023-06-29 14:10:50 -0500

    Cherno tutorial on textures added texture's class to input (read buffer using OpenGL) .png files. Also refactored Shader program to take in those new uniforms

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -13,6 +13,7 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "Texture.h"
 
 //struct ShaderProgramSource
 //{
@@ -146,10 +147,10 @@ int main(void)
 
     {
         float positions[] = {
-            -.5f, -.5f,
-             .5f, -.5f,
-             .5f,  .5f,
-            -.5f,  .5f
+            -.5f, -.5f, 0.0f, 0.0f,
+             .5f, -.5f, 1.0f, 0.0f,
+             .5f,  .5f, 1.0f, 1.0f,
+            -.5f,  .5f, 0.0f, 1.0f
         };
 
         unsigned int indicies[] = {
@@ -157,11 +158,14 @@ int main(void)
             2, 3, 0
         };
 
-        
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
         VertexArray va;
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
         VertexBufferLayout layout;
+        layout.Push<float>(2);
         layout.Push<float>(2);
         va.AddBuffer(vb, layout);
 
@@ -170,6 +174,10 @@ int main(void)
         Shader shader("res/shaders/Basic.shader");  
         shader.Bind();
         shader.SetUniform4f("u_Color", .0f, 1.0f, .0f, 1.0f);
+
+        Texture texture("res/textures/anyaT.png");
+        texture.Bind();
+        shader.SetUniform1i("u_Texture", 0);
 
         va.Unbind();
         vb.Unbind();
