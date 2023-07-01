e2e46025669843ab1cc3367e83c3591ac721e7bd
spiffy07 <thomas.tran0789>
2023-06-30 19:28:39 -0500

    Finished implementing test that renders two identical textures (as from Cherno's multiple objects video). refactored most objects out of main()

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -22,6 +22,7 @@
 #include "imgui/imgui_impl_glfw_gl3.h"
 
 #include "Tests/TestClearColor.h"
+#include "Tests/TestTexture.h"
 
 int main(void)
 {
@@ -43,9 +44,8 @@ int main(void)
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
+    glfwSwapInterval(1);            // sets frame rate to monitor's refresh
 
     if (glewInit() != GLEW_OK)
         std::cout << "Error!\n";
@@ -53,55 +53,8 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     {
-        {
-            //float positions[] = {
-            //    -150.0f,      -150.0f,    0.0f,   0.0f,
-            //     150.0f,      -150.0f,    1.0f,   0.0f,
-            //     150.0f,       150.0f,    1.0f,   1.0f,
-            //    -150.0f,       150.0f,    0.0f,   1.0f
-            //};
-
-            //unsigned int indicies[] = {
-            //    0, 1, 2,
-            //    2, 3, 0
-            //};
-        }
-
         GLCall(glEnable(GL_BLEND));
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-        {
-            //VertexArray va;
-            //VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-            //VertexBufferLayout layout;
-            //layout.Push<float>(2);
-            //layout.Push<float>(2);
-            //va.AddBuffer(vb, layout);
-
-            //IndexBuffer ib(indicies, 6);
-
-            ////glm::mat4 proj = glm::ortho(-.5f, .5f, -.375f, .375f, -1.0f, 1.0f);
-            //glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 640.0f, -1.0f, 1.0f);             // glm to set aspect ratio
-            //glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));  // move "camera" so negate
-
-
-            //Shader shader("res/shaders/Basic.shader");      // path to shader file
-            //shader.Bind();
-            //shader.SetUniform4f("u_Color", .0f, 1.0f, .0f, 1.0f);
-
-            //Texture texture("res/textures/anyaT.png");      // input .png
-            //texture.Bind();
-            //shader.SetUniform1i("u_Texture", 0);
-
-            //va.Unbind();
-            //vb.Unbind();
-            //ib.Unbind();
-            //shader.Unbind();
-
-            //glm::vec3 translationA(0.0f, 0.0f, 0.0f);
-            //glm::vec3 translationB(400.0f, 400.0f, 0.0f);
-        }
 
         Renderer renderer;
 
@@ -111,7 +64,10 @@ int main(void)
 
         test::TestClearColor test;
 
-        /* Loop until the user closes the window  */
+        //test::TestTexture test;
+        //test.SetRenderer(renderer);
+
+        /* Loop until the user closes the window    -------------------------------------------*/
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
@@ -123,41 +79,11 @@ int main(void)
             ImGui_ImplGlfwGL3_NewFrame();
             test.OnImGuiRender();
 
-            {
-                //{
-                //    glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA); // move model
-                //    glm::mat4 mvp = proj * view * model;
-                //    shader.Bind();
-                //    shader.SetUniformMat4f("u_MVP", mvp);                  // for fixing with aspect ratio
-
-                //    renderer.Draw(va, ib, shader);
-                //}
-
-                //{
-                //    glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB); // move model
-                //    glm::mat4 mvp = proj * view * model;
-                //    shader.Bind();
-                //    shader.SetUniformMat4f("u_MVP", mvp);                  // for fixing with aspect ratio
-
-                //    renderer.Draw(va, ib, shader);
-                //}
-         
-
-                //{
-                //    ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);  
-                //    ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);  
-
-                //    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-                //}
-            }
-
             ImGui::Render();
             ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
-            /* Swap front and back buffers */
             glfwSwapBuffers(window);
 
-            /* Poll for and process events */
             glfwPollEvents();
         }
     }
--- OpenGL/src/Tests/TestTexture.h
+++ OpenGL/src/Tests/TestTexture.h
@@ -2,6 +2,14 @@
 
 #include "Tests.h"
 
+#include "Renderer.h"
+#include "VertexArray.h"
+#include "VertexBufferLayout.h"
+#include "Texture.h"
+
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/glm.hpp"
+
 namespace test {
 
 	class TestTexture : public Test
@@ -14,7 +22,28 @@ namespace test {
 		void OnRender() override;
 		void OnImGuiRender() override;
 
-	private:
+		void SetRenderer(Renderer& renderer);
 
+		inline VertexArray& GetVA() { return m_Va; };
+		inline IndexBuffer& GetIB() { return m_Ib; };
+		inline Shader& GetShader() { return m_Shader; };
+
+	private:
+		float m_Positions[16];
+		unsigned int m_Indicies[6];
+
+		VertexBuffer m_Vb;
+		VertexArray m_Va;
+		VertexBufferLayout m_Layout;
+		IndexBuffer m_Ib;
+		Shader m_Shader;
+		Texture m_Texture;      // input .png
+		Renderer* m_RendererPtr;
+
+		glm::mat4 m_Proj;
+		glm::mat4 m_View;
+
+		glm::vec3 m_TranslationA;
+		glm::vec3 m_TranslationB;
 	};
 }
